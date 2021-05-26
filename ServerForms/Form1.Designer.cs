bce4bf80ba693478703ee02da9d8a9b98e5581f7
Felicia Stenvall <felicia.stenvall2244@gmail.com>
2021-05-26 09:26:28 +0200

    Add project files.

---
+++ ServerForms/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace ServerForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tbxIPAdress = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rtbMessage = new System.Windows.Forms.RichTextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IP-adress";
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Location = new System.Drawing.Point(364, 78);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 27);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbxIPAdress
+            // 
+            this.tbxIPAdress.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.tbxIPAdress.Location = new System.Drawing.Point(159, 78);
+            this.tbxIPAdress.Margin = new System.Windows.Forms.Padding(4);
+            this.tbxIPAdress.Name = "tbxIPAdress";
+            this.tbxIPAdress.Size = new System.Drawing.Size(178, 32);
+            this.tbxIPAdress.TabIndex = 2;
+            this.tbxIPAdress.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rtbMessage);
+            this.groupBox1.Controls.Add(this.btnSend);
+            this.groupBox1.Location = new System.Drawing.Point(30, 185);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(473, 216);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Message";
+            // 
+            // rtbMessage
+            // 
+            this.rtbMessage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.rtbMessage.Cursor = System.Windows.Forms.Cursors.Default;
+            this.rtbMessage.Location = new System.Drawing.Point(18, 31);
+            this.rtbMessage.Name = "rtbMessage";
+            this.rtbMessage.Size = new System.Drawing.Size(436, 131);
+            this.rtbMessage.TabIndex = 1;
+            this.rtbMessage.Text = "";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(331, 168);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(136, 42);
+            this.btnSend.TabIndex = 0;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(535, 419);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.tbxIPAdress);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Client";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tbxIPAdress;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox rtbMessage;
+        private System.Windows.Forms.Button btnSend;
+    }
+}
+
---
+++ ServerForms/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ServerForms
+{
+    public partial class Form1 : Form
+    {
+        TcpClient   klient;
+        int         port = 12345;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            IPAddress address = IPAddress.Parse(tbxIPAdress.Text);
+            klient = new TcpClient();
+            klient.NoDelay = true;
+            klient.Connect(address, port);
+
+            if (klient.Connected)
+            {
+                byte[] utData = Encoding.Unicode.GetBytes(rtbMessage.Text);
+                klient.GetStream().Write(utData, 0, utData.Length);
+                klient.Close();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
